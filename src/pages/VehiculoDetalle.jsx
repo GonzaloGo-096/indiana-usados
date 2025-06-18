4262c5ef8e96658fa0d6383f27e25ea65570bf4a
Gonzalo GO <gonzalo.gomezomil01@gmail.com>
2025-06-18 13:21:30 -0300

    este es un commito que saca el skelton del de detallesVehiculos , y luego lo importa. busco limpieza de codigo y modularidad

--- src/pages/VehiculoDetalle.jsx
+++ src/pages/VehiculoDetalle.jsx
@@ -3,47 +3,7 @@ import { useParams, Link } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import autoService, { queryKeys } from '../service/service'
 import img from '..//assets/auto1.jpg'  // Importar la imagen
-
-// Componente Skeleton para el detalle
-const DetalleSkeleton = () => (
-    <div className="container py-5">
-        <div className="row">
-            <div className="col-md-6">
-                <div className="card shadow-sm">
-                    <div className="placeholder-glow">
-                        <div className="placeholder w-100" style={{ height: '400px' }}></div>
-                    </div>
-                </div>
-            </div>
-            <div className="col-md-6">
-                <div className="card shadow-sm h-100">
-                    <div className="card-body">
-                        <div className="placeholder-glow">
-                            <h2 className="card-title placeholder col-8 mb-4"></h2>
-                            <div className="d-flex justify-content-between mb-3">
-                                <div className="placeholder col-4"></div>
-                                <div className="placeholder col-4"></div>
-                            </div>
-                            <div className="d-flex justify-content-between mb-3">
-                                <div className="placeholder col-3"></div>
-                                <div className="placeholder col-3"></div>
-                            </div>
-                            <div className="d-flex justify-content-between mb-3">
-                                <div className="placeholder col-5"></div>
-                                <div className="placeholder col-5"></div>
-                            </div>
-                            <div className="d-flex justify-content-between mb-3">
-                                <div className="placeholder col-4"></div>
-                                <div className="placeholder col-4"></div>
-                            </div>
-                            <div className="placeholder col-12 mt-4" style={{ height: '100px' }}></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-)
+import DetalleSkeleton from '../components/skeletons/DetalleSkeleton'
 
 const VehiculoDetalle = () => {
     const { id } = useParams()
